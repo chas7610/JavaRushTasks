51da2436e521475adf87633cf716902863961594
chas7610 <chas7610@gmail.com>
2017-06-20 11:29:46 +0300

    test Git

--- 2.JavaCore/src/com/javarush/task/TestAdapter.java
+++ 2.JavaCore/src/com/javarush/task/TestAdapter.java
@@ -7,6 +7,8 @@ import java.io.Reader;
  * Created by cherevko-vv on 20.06.2017.
  */
 public class TestAdapter {
-
+    public static void main(String[] args) {
+        System.out.println("test Git");
+    }
 }
 
