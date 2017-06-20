de3fb2e119b0f398cbf00fefdfcf520d52c88ffd
chas7610 <chas7610@gmail.com>
2017-06-20 13:18:49 +0300

    task1911

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task19.task1913;
+
+/* 
+Выводим только цифры
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(byteArrayOutputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(out);
+        for (int i = 0; i < byteArrayOutputStream.size(); i++){
+            if (Character.isDigit(byteArrayOutputStream.toByteArray()[i]))
+                System.out.print((char) byteArrayOutputStream.toByteArray()[i]);
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
