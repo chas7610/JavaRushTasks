2a4f8550955b41a684b24c588147c07dcf384bee
chas7610 <chas7610@gmail.com>
2017-06-21 17:30:40 +0300

    task1911

--- 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -3,11 +3,9 @@ package com.javarush.task.task19.task1917;
 /* 
 Свой FileWriter
 */
-
 import java.io.*;
 
-public class FileConsoleWriter{
-
+public class FileConsoleWriter {
     public FileConsoleWriter(String fileName) throws IOException{
         fileWriter = new FileWriter(fileName);
     }
